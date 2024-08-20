a352a28048691e6d523c5e406097481a7db31be8
firsthandcraft <opaldust19@gmail.com>
2024-08-20 23:10:45 +0900

    dd

--- js/main.js
+++ js/main.js
@@ -6,3 +6,26 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('transparent');
     }
 });
+document.addEventListener('DOMContentLoaded', function() {
+    var aboutLink = document.getElementById('about-link');
+    var newPage = document.getElementById('new-page');
+    var closeModal = document.getElementById('close-modal');
+
+ // About 링크 클릭 시 모달 열기/닫기
+    aboutLink.addEventListener('click', function(event) {
+        event.preventDefault(); // 기본 링크 동작을 막음
+        newPage.classList.toggle('hidden'); // 모달창을 열거나 닫음 (토글)
+    });
+
+    // 닫기 버튼 클릭 시 모달 닫기
+    closeModal.addEventListener('click', function() {
+        newPage.classList.add('hidden'); // 모달창을 숨김
+    });
+
+    // 모달창 밖을 클릭하면 모달 닫기
+    window.addEventListener('click', function(event) {
+        if (event.target == newPage) {
+            newPage.classList.remove('hidden'); // 모달창을 숨김
+        }
+    });
+});
